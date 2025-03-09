a329fc7e85097568818aa0328141bfcc371dc3c3
Ashley Le <ashleyle@usf.edu>
2025-03-10 08:52:13 +0900

    Task 2 - Employee Cards Dynamic Addition

--- cc_13.js
+++ cc_13.js
@@ -1,1 +1,31 @@
 //Task 1: Creating the Base Structure in index.html
+
+//Task 2: Adding Employee Cards Dynamically
+function createEmployeeCard(name, position) {
+    const employeeCard = document.createElement('div');
+employeeCard.setAttribute(`class`, `employee-card`);
+
+    const employeeContainer = document.getElementById(`.employeeContainer`);
+employeeContainer.appendChild(employeeCard);
+
+    const header = document.createElement(`h2`);
+    header.textContent = name;
+
+    const paragraph = document.createElement(`p`);
+    paragraph.textContent = position;
+
+    const removeButton = document.createElement(`button`);
+    removeButton.textContent = `Remove`;
+    removeButton.addEventListener(`click`, () => {
+        employeeCard.remove();
+});
+
+employeeCard.appendChild(header);
+employeeCard.appendChild(paragraph);
+employeeCard.appendChild(removeButton);
+};
+
+    //Task 2: Test Cases
+createEmployeeCard(`Mary Poppins`, `Umbrella Engineer`);
+createEmployeeCard(`Curious George`, `Banana Specialist`);    
+createEmployeeCard(`The Rock`, `Rock`);
